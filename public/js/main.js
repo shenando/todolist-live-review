85ca4ceb2b0b2dd66ba573b606fe1cd256313c9b
Shenandoah Grace <69861032+shenando@users.noreply.github.com>
2021-04-06 19:46:07 -0400

    Update main.js
    Comments for main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,63 @@
-const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('.todoItem span')
-const todoComplete = document.querySelectorAll('.todoItem span.completed')
+const deleteBtn = document.querySelectorAll('.del') //bring in delete button
+const todoItem = document.querySelectorAll('.todoItem span') //bring to do button
+const todoComplete = document.querySelectorAll('.todoItem span.completed') //bring in any button that has a class of completed
 
-Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+Array.from(deleteBtn).forEach((el)=>{ //Add an event listener to every delete button
+    el.addEventListener('click', deleteTodo) //it will run th deleteTodo function
 })
 
-Array.from(todoItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
+Array.from(todoItem).forEach((el)=>{ //Add an event listener to every to do item
+    el.addEventListener('click', markComplete) //it will run the markComplete function
 })
 
-Array.from(todoComplete).forEach((el)=>{
-    el.addEventListener('click', undo)
+Array.from(todoComplete).forEach((el)=>{ //add event listener to every to do item with the complete task
+    el.addEventListener('click', undo) //it will run the undo function
 })
 
-async function deleteTodo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
+async function deleteTodo(){ //async function
+    const todoText = this.parentNode.childNodes[1].innerText //bring in the todo text from the database
+    try{ //the first thing the code will try to do
+        const response = await fetch('deleteTodo', { //await the response from the database and run the delete method that's in server.js
+            method: 'delete', //the method that's being run
+            headers: {'Content-type': 'application/json'}, //the type of data in the object
+            body: JSON.stringify({ //the body of the data in the data base in json form
+                'rainbowUnicorn': todoText //the todo that was brought in and will be deleted 
+            })
+        })
+        const data = await response.json() //the data that is brought in
+        console.log(data)
+        location.reload() //refresh the page
+    }catch(err){
+        console.log(err) //catches any errors
+    }
+}
+
+async function markComplete(){ //async function to mark completed items 
+    const todoText = this.parentNode.childNodes[1].innerText //bring in the specific item 
+    try{ //first thing the block of code will try
+        const response = await fetch('markComplete', { //wait for the database to return this info and then it will run the method from server.js
+            method: 'put', //the type of method - it will be putting a mark on the todo
+            headers: {'Content-type': 'application/json'}, //type of data
+            body: JSON.stringify({ //the data itself in json form
                 'rainbowUnicorn': todoText
             })
         })
         const data = await response.json()
         console.log(data)
-        location.reload()
+        location.reload() //refresh page
     }catch(err){
-        console.log(err)
+        console.log(err) //catches any errors and reports what kind
     }
 }
 
-async function markComplete(){
-    const todoText = this.parentNode.childNodes[1].innerText
+async function undo(){ //async to undo the mark as complete action
+    const todoText = this.parentNode.childNodes[1].innerText //taking in the specific data
     try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
+        const response = await fetch('undo', { //waits for data from database, then takes in action from server js
+            method: 'put', //type of method, it will be un putting the styling
+            headers: {'Content-type': 'application/json'}, //type of data
             body: JSON.stringify({
-                'rainbowUnicorn': todoText
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
-}
-
-async function undo(){
-    const todoText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('undo', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'rainbowUnicorn': todoText
+                'rainbowUnicorn': todoText //data itself
             })
         })
         const data = await response.json()
